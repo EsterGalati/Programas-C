53dbdcd3bbced7064e0dd7d5f705f08f2cb6199c
EsterGalati <esterscgalati@gmail.com>
2022-10-24 10:06:15 -0300

    Jogo de Adivinhação

---
+++ Adivinhacao.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("*****************************************\n");
+    printf("*BEM VINDOS AO NOSSO JOGO DE ADIVINHACAO*\n");
+    printf("*****************************************\n");
+} 
