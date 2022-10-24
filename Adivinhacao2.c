2e1e27924e94afd13854b31aebf09ab1b11573cf
EsterGalati <esterscgalati@gmail.com>
2022-10-24 20:12:04 -0300

    Jogo de Adivinhação

---
+++ Adivinhacao2.c
@@ -0,0 +1,247 @@
+//Programa feio poo Caio Manoel Costa Leonardo
+#include <stdio.h>
+
+
+int main(){
+ 
+int n1, n2 , n3, n4, n5;
+char a[999], b[999];
+
+
+printf("Fala jogador,me diga,qual seu nome?\n");
+scanf("%s", &a);
+printf("Duvida eu descobrir qual '@' voce vai escolher,%.s?\nNao acredita KKKK,entao vamos la:\n\n", a);
+printf("Escolha ai qual casa com '@' voce acha que eu nao vou conseguir descobrir:\n\n\n");
+
+
+
+
+
+
+                                                    //aqui
+
+                                            //aqui            //aqui
+
+                                            //aqui            //aqui
+
+                                                    //aqui
+
+
+
+
+
+
+
+
+
+printf("\n\n\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t|casa A|\n");
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t   @\n\n\n");
+
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t|casa B|\t\t\t|casa F|\n");
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t   @\t\t\t\t   @\n\n\n");
+
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t|casa C|\t\t\t|casa E|\n");
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t   @\t\t\t\t   @\n\n\n");
+
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t|casa D|\n");
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t   @\n\n\n\n");
+
+
+printf("Escolheu?Toppp\nPreste atencao,os '@' seram substituidos por numeros\n\n");
+printf("se quer continuar digite 1,caso contrario digite outro numero\n\n\n");
+scanf("%i", &n3);
+
+switch (n3)
+{
+case 1: 
+
+
+printf("\n\n\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t|casa A|\n");
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t   4\n\n\n");
+
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t|casa B|\t\t\t|casa F|\n");
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t   3\t\t\t\t   5\n\n\n");
+
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t|casa C|\t\t\t|casa E|\n");
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t   2\t\t\t\t   6\n\n\n");
+
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t|casa D|\n");
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t   1\n\n\n\n");
+
+
+
+printf("Qual numero escolheu,%s?\n\n", a);
+scanf("%i", &n4);
+
+switch (n4)
+{
+case 1:
+
+printf("aqui vai uma curiosidade:\no numero 1 No Egito, por exemplo, por volta do ano 3000 a.C, o numero 1 era um elemento essencial de medida das grandes construcoes egipcias\n\n\n\n\n");
+
+
+    break;
+
+case 2:
+
+printf("aqui vai uma curiosidade:\nO numero 2 e o unico numero (alem do 0) em que n+n = 2n = (n) ao quadrado) , e o unico numero primo par alem de ser um numero da sequencia de Fibonacci\n\n\n\n\n");
+
+
+    break;
+
+    case 3:
+
+printf("aqui vai uma curiosidade:\nFazer 3 fogueiras e um pedido de socorro, pois 3 eh um numero mundial.\n\n\n\n\n");
+
+
+    break;
+
+    case 4:
+
+printf("aqui vai uma curiosidade:\nNa Rosa dos Ventos, os quatro pontos cardeais sao as quatro direcoes essenciais: Norte, Sul, Leste, Oeste.\n\n\n\n\n");
+
+
+    break;
+
+    case 5:
+
+printf("aqui vai uma curiosidade:\nO numero cinco tambem eh entendido como um numero de Fibonacci.\n");
+printf("Se quiser saber oque eh um nemero de Fibonacci digite 1,se nao quiser digite qualquer numero tecla\n\n");
+scanf("%i", &n5);
+
+switch (n5)
+{
+case 1 :
+
+printf("O numero cinco tambem eh entendido como um numero de Fibonacci. Mas o que e a sequencia de Fibonacci? Essa sequencia foi proposta pelo matematico Leonardo Pisa. Em sintese, os numeros da sequencia de Fibonacci sao encontrados dentro da matematica com mais frequencia do que ha qualquer explicacao para esse fato atipico. Alem disso, esses numeros tambem sao encontrados na natureza. Inclusive, o exemplo mais classico dessa anomalia matematica deu-se utilizando a reproducao dos coelhos que se comporta conforme a sequencia de Fibonacci (1, 1, 2, 3, 5, 8, etc), onde o numero e igual a soma dos dois anteriores.\n\n\n\n\n");
+
+  break;
+
+default:  break;
+}
+
+    break;
+
+    case 6:
+
+printf("aqui vai uma curiosidade:\nNem todos podem ser, mas o 6 e um numero perfeito.Os numeros perfeitos sao iguais a soma de seus divisores: 6 pode ser dividido por 1, 2 e 3 e, quando voce soma esses numeros, o resultado e 6.\n\n\n\n\n");
+
+
+    break;
+
+
+default:printf("numero invalido %s,tente de novo ou vai perder uma curiosidade muito legal\n", a);
+    break;
+}
+
+printf("Certo,agora vc vai andar a quantidade de numeros que tiver nessa casa no sentido anti-horario!!(mais sem roubar em)\n");
+
+printf("\n\n\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t|casa A|\n");
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t   4\n\n\n");
+
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t|casa B|\t\t\t|casa F|\n");
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t   3\t\t\t\t   5\n\n\n");
+
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t|casa C|\t\t\t|casa E|\n");
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t   2\t\t\t\t   6\n\n\n");
+
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t|casa D|\n");
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t   1\n\n\n\n");
+
+
+printf("Andou?\n");
+printf("beleza,agora vou fazer minha magica\n");
+printf("agora me diga ums palavra magica:\n");
+scanf("%s", &b);
+
+printf("1 %s\n", b);
+printf("2 %s\n", b);
+printf("3 %s\n", b);
+printf("\a");
+
+printf("\n\n\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t|casa A|\n");
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t  :p\n\n\n");
+
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t|casa B|\t\t\t|casa F|\n");
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t   :c\t\t\t\t   :<\n\n\n");
+
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t|casa C|\t\t\t|casa E|\n");
+printf("\t\t\t--------\t\t\t--------\n");
+printf("\t\t\t   :m\t\t\t\t   <3\n\n\n");
+
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t|casa D|\n");
+printf("\t\t\t\t\t--------\n");
+printf("\t\t\t\t\t   :v\n\n\n\n");
+
+
+
+printf("A casa que voce parou foi a 'E' com o emoji '<3' ne,KKKK\nTe falei que ia adivinhar");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ break;
+
+
+
+default:printf("Entendi,nao quer jogar,tudo bem,tenha um otimo dia ou final de dia,nao sei quando veio jogar K");break;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
