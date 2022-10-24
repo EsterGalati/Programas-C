9206c2250a2de6025b85f76ce9b45883085f2072
EsterGalati <esterscgalati@gmail.com>
2022-10-24 18:37:22 -0300

    Jogo de Adivinhação de Numero

--- Adivinhacao.c
+++ Adivinhacao.c
@@ -8,45 +8,46 @@ int main(){
 
     int numerosecreto = 42;
     int chute;
-    int i;
+    int i; // Variavel para o loop
     for ( i = 1; i <=NUMERO_DE_TENTATIVAS; i++ ){
+      
+      
    
     
         printf("Tentativa %d de %d\n",i, NUMERO_DE_TENTATIVAS);
         printf("Qual e o seu chute? ");
+        
         scanf("%d", &chute);
         printf("Seu chute foi %d\n",chute);
-
+            if (chute < 0)
+            {
+            printf("Não pode numeros negativos\n");
+            }
+            
     
         int acertou = (chute==numerosecreto);
+        int maior = chute>numerosecreto;
+        int menor = chute<numerosecreto;
         if (acertou){
             printf("Parabens!Voce acertou\n");
             printf("Jogue novamente!!\n");
             break;
         }
-        else{
+        else if (maior){
 
-            int maior = chute>numerosecreto;
-            if (maior){
                 printf("Seu chute foi maior do que o numero secreto\n");
             }
-            else{
+        else if (menor){
+
                 printf("Seu numero foi menor do que o numero secreto\n");
-            }
+        }
+        
             
             
         }
         
-
-
-
-
-
-
-
-
-
+        printf("FIM DE JOGO!!\n");
         }
-    printf("FIM DE JOGO!!\n");
-    } 
+   
+    
     
