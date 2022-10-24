00ba06830e5a5bc59ca507e9217c750cd178b5f3
EsterGalati <esterscgalati@gmail.com>
2022-10-24 20:12:24 -0300

    Jodo de Adivinhação de Numero.

--- Adivinhacao.c
+++ Adivinhacao.c
@@ -1,27 +1,32 @@
 #include<stdio.h>
-#define NUMERO_DE_TENTATIVAS 5
+
+
 int main(){
     // Imprime o cabecalho do nosso jogo.
+   
     printf("*****************************************\n");
     printf("*BEM VINDOS AO NOSSO JOGO DE ADIVINHACAO*\n");
     printf("*****************************************\n");
 
     int numerosecreto = 42;
     int chute;
-    int i; // Variavel para o loop
-    for ( i = 1; i <=NUMERO_DE_TENTATIVAS; i++ ){
-      
-      
-   
-    
-        printf("Tentativa %d de %d\n",i, NUMERO_DE_TENTATIVAS);
+    int ganhou=0; // variavel boleana nesse caso 0 significa falso
+    int tentativas=0;
+    while (ganhou == 0){ // Utilizamos while quando não sabemos o numero de tentativas que serão feitas,
+                        // Enquanto no for sabemos extamente quantas vezes queremos que o codigo repita.
+        
+
+        printf("Tentativa %d \n",tentativas+1);
         printf("Qual e o seu chute? ");
         
         scanf("%d", &chute);
         printf("Seu chute foi %d\n",chute);
             if (chute < 0)
             {
-            printf("Não pode numeros negativos\n");
+            printf("Nao pode numeros negativos\n");
+           
+            continue; 
+
             }
             
     
@@ -29,9 +34,9 @@ int main(){
         int maior = chute>numerosecreto;
         int menor = chute<numerosecreto;
         if (acertou){
-            printf("Parabens!Voce acertou\n");
-            printf("Jogue novamente!!\n");
-            break;
+            printf("Parabens!Voce acertou,\n");
+            
+            ganhou=1;
         }
         else if (maior){
 
@@ -43,9 +48,11 @@ int main(){
         }
         
             
-            
+            tentativas++; // Abreviação para tentativas=tentativas+1
         }
         
+        
+        printf("Voce acertou em %d tentativas\n",tentativas);
         printf("FIM DE JOGO!!\n");
         }
    
