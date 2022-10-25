11d4292b721b7eda92869b0a4bdc611b46fe9966
EsterGalati <esterscgalati@gmail.com>
2022-10-25 20:22:28 -0300

    Jogo de Adivinhação de Numero

--- Adivinhacao.c
+++ Adivinhacao.c
@@ -7,11 +7,16 @@ int main(){
     printf("*****************************************\n");
     printf("*BEM VINDOS AO NOSSO JOGO DE ADIVINHACAO*\n");
     printf("*****************************************\n");
-
+    printf("Voce iniciara o jogo com 1000 pontos, evite perder pontos acertando qual sera o numero secreto <3\n");
     int numerosecreto = 42;
     int chute;
+
     int ganhou=0; // variavel boleana nesse caso 0 significa falso
     int tentativas=0;
+
+    int pontos = 1000;
+    
+
     while (ganhou == 0){ // Utilizamos while quando não sabemos o numero de tentativas que serão feitas,
                         // Enquanto no for sabemos extamente quantas vezes queremos que o codigo repita.
         
@@ -49,10 +54,17 @@ int main(){
         
             
             tentativas++; // Abreviação para tentativas=tentativas+1
+
+            int pontosperdidos=(chute-numerosecreto)/2;
+            if (pontosperdidos<0) pontosperdidos = pontosperdidos*(-1); 
+            pontos = pontos - pontosperdidos;
+
+
         }
         
         
         printf("Voce acertou em %d tentativas\n",tentativas);
+        printf("Voce alcancou %d pontos, parabens\n",pontos);
         printf("FIM DE JOGO!!\n");
         }
    
